5dcc89e6faaf2f4f512feed2083409591e29a236
Agusmayans <gallemayans@gmail.com>
2017-04-03 19:39:01 -0300

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,15 +1,12 @@
-# hello.c 
-/*
+ /*
  * Imprimir hello world en pantallla.
  * Agustin Mayans
  * 14/13/17
  */
 
 #include <stdio.h>
-#include <stdlib.h>
 
 int main()
 {
     printf("Hello world!\n");
-    return 0;
 }
